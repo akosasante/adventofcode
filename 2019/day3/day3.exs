df2cb31c5116520fb956aaee9a1d11d3d5280129
Akosua Asante <akosuaasante@gmail.com>
2019-12-05 11:10:31 -0800

    little bit of cleanup

--- 2019/day3/day3.exs
+++ 2019/day3/day3.exs
@@ -5,9 +5,9 @@ defmodule Day3 do
     def run_brute() do
       InputParser.get_input_stream_for_day(3)
       |> Enum.map(&(String.split(&1, ",")))
+      |> do_the_thing
     end
-    def do_the_thing([wire1, wire2] = instruction_list) do
-#      IO.inspect(instruction_list)
+    def do_the_thing([wire1, wire2]) do
       wire1steps = []
       wire2steps = []
       start_position = {0, 0}
@@ -19,20 +19,19 @@ defmodule Day3 do
       IO.puts("Wire1 end: #{inspect wire1_end_position}; Wire2 end: #{inspect wire2_end_position}")
       
       intersections = MapSet.intersection(MapSet.new(wire1steps), MapSet.new(wire2steps))
-#      IO.inspect(wire1steps)
-#      IO.inspect(wire2steps)
       IO.puts("Intersections: #{inspect intersections}")
       
       {min_x, min_y} = Enum.min_by(intersections, fn {x_val, y_val} = intersection -> 
-        IO.puts("Intersection: #{inspect intersection}; Distance=#{x_val + y_val}")
-        x_val + y_val end)
-      min_x + min_y
+        sum = abs(x_val) + abs(y_val)
+        IO.puts("Intersection: #{inspect intersection}; Distance=#{sum}")
+        sum
+      end)
+      abs(min_x) + abs(min_y)
     end
     
     defp run_instruction_set(instructions, start_position, wire_steps) do
       Enum.reduce(instructions, {start_position, wire_steps}, fn instruction, {new_curr_pos, new_steps} ->
         IO.puts("instruction: #{instruction}, starting from : #{inspect new_curr_pos}")
-        #        IO.puts("acc: #{inspect acc}")
         case instruction do
           "R" <> num_steps -> compute_movement(new_curr_pos, new_steps, String.to_integer(num_steps), 0)
           "L" <> num_steps -> compute_movement(new_curr_pos, new_steps, -(String.to_integer(num_steps)), 0)
@@ -49,18 +48,17 @@ defmodule Day3 do
           skip_val = elem(curr_position, 1)
           Enum.map(elem(curr_position, 1)..(elem(curr_position, 1) + y_axis_steps),  fn 
             val when val !=skip_val -> {elem(curr_position, 0), val}
-            val -> nil
+            _ -> nil
           end) |> Enum.reject(&is_nil/1)
         _ -> 
           skip_val = elem(curr_position, 0)
           Enum.map(elem(curr_position, 0)..(elem(curr_position, 0) + x_axis_steps),  fn 
             val when val !=skip_val -> {val, elem(curr_position, 1)} 
-            val -> nil
+            _ -> nil
           end) |> Enum.reject(&is_nil/1)
       end
       wire_steps = Enum.concat(wire_steps, movement)
       {new_position, wire_steps}
-#      |> IO.inspect
     end
     
     defp update_curr_position(curr_position, x_axis_steps, y_axis_steps) do
