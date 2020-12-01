65b50bb1c620e4e34d98b9d995445b9ed4eb0dc2
Akosua Asante <akosuaasante@gmail.com>
2020-12-01 17:28:03 -0500

    Map solution for day 1 part 1, doesn't change benchmarks much

--- 2020/lib/day1/code.ex
+++ 2020/lib/day1/code.ex
@@ -3,31 +3,50 @@
 
     def part1(input) do
       log("Running 2020-1-P1-InputList")
-      log("Input length: #{length(input)}")
-      valid_nums = input
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.reject(&(&1 > 2020))
+      valid_num_map =
+        input
+        |> Enum.map(fn val -> {val, String.to_integer(val)} end)
+        |> Map.new
 
-      log("Filtered input #{length(valid_nums)}")
       try do
-        for num <- valid_nums do
-          case Enum.find(valid_nums, fn other -> other + num == 2020 end) do
+        for num <- input do
+          val = String.to_integer(num)
+          case Map.get(valid_num_map, 2020 - val) do
             nil ->
-              log("#{num} did not sum to 2020 with any numbers")
               nil
             matching_val ->
-              log("#{num}+#{matching_val}=2020, returning product")
-              throw(matching_val * num)
+              throw(matching_val * val)
           end
         end
       catch
         result -> result
       end
+
+#      valid_nums = input
+#      |> Enum.map(&String.to_integer/1)
+#      |> Enum.reject(&(&1 > 2020))
+#
+#      try do
+#        for num <- valid_nums do
+#          case Enum.find(valid_nums, fn other -> other + num == 2020 end) do
+#            nil ->
+#              nil
+#            matching_val ->
+#              throw(matching_val * num)
+#          end
+#        end
+#      catch
+#        result -> result
+#      end
     end
 
     def part1_stream(input_stream) do
-      log("Running 2020-1-P1-InputStream")
-      "Not implemented"
+#      log("Running 2020-1-P1-InputStream")
+#      input_stream
+#      |> Stream.map(&String.to_integer/1)
+#      |> Stream.reject(&(&1 > 2020))
+#      |> Stream.take_while(fn num -> Enum.find() end)
+#      "Not implemented"
     end
 
     def part2(input) do
