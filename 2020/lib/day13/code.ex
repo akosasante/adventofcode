b66a0023f9df7420797ba1ba6ed028ded9d1218a
Akosua Asante <akosuaasante@gmail.com>
2020-12-13 21:50:58 -0500

    day 13

---
+++ 2020/lib/day13/code.ex
@@ -0,0 +1,33 @@
+  defmodule Advent2020.Day13 do
+    import Helpers.Shared, only: [log: 1]
+
+    def part1(input) do
+      log("Running 2020-13-P1-InputList")
+      "Not implemented"
+    end
+
+    def part1_optimized(input) do
+      log("Running 2020-13-P1-InputListOptimized")
+      "Not implemented"
+    end
+
+    def part1_stream(input_stream) do
+      log("Running 2020-13-P1-InputStream")
+      "Not implemented"
+    end
+
+    def part2(input) do
+      log("Running 2020-13-P2-InputList")
+      "Not implemented"
+    end
+
+    def part2_optimized(input) do
+      log("Running 2020-13-P2-InputListOptimized")
+      "Not implemented"
+    end
+
+    def part2_stream(input_stream) do
+      log("Running 2020-13-P2-InputStream")
+      "Not implemented"
+    end
+  end
