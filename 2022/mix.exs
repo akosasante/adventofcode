3a591637340235d6b067eb2451b6305be1ec39ed
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2022-12-01 12:53:04 -0500

    start 2022 folder

---
+++ 2022/mix.exs
@@ -0,0 +1,28 @@
+defmodule Advent2022.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent_2022,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:benchee, "~> 1.1.0"},
+      {:advent_of_code_helper, "~> 0.2.1"}
+    ]
+  end
+end
