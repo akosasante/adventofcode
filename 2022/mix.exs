5f63d3ceb62578f5019c35447842a34c04987393
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2022-12-10 20:59:16 -0500

    commit days 6 & 7

--- 2022/mix.exs
+++ 2022/mix.exs
@@ -21,7 +21,8 @@ defmodule Advent2022.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:advent_of_code_helper, "~> 0.3.1"}
+      {:advent_of_code_helper, "~> 0.3.1"},
+      {:libgraph, "~> 0.16.0"}
     ]
   end
 end
